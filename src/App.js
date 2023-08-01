d03077e89b1e4569c0531aa8c3f19823badea355
Kadiraydn01 <kadiraydn01@gmail.com>
2023-08-01 16:43:55 +0300

    App.js bakıldı

--- src/App.js
+++ src/App.js
@@ -4,38 +4,37 @@
   Nerelerde değişiklik yapmanız gerektiğini anlamak için her dosyayı inceleyin.
 */
 
-// State hook u import edin
-import React from "react";
-
-// Gönderiler (çoğul!) ve AramaÇubuğu bileşenlerini import edin, çünkü bunlar App bileşeni içinde kullanılacak
-// sahteVeri'yi import edin
+import React, { useState } from "react";
+import "./bilesenler/AramaCubugu";
+import "./bilesenler/Gonderiler";
 import "./App.css";
+import sahteVeri from "./sahte-veri";
 
 const App = () => {
-  // Gönderi nesneleri dizisini tutmak için "gonderiler" adlı bir state oluşturun, **sahteVeri'yi yükleyin**.
-  // Artık sahteVeri'ye ihtiyacınız olmayacak.
-  // Arama çubuğunun çalışması için , arama kriterini tutacak başka bir state'e ihtiyacımız olacak.
+  const [gonderiler, setGonderiler] = useState(sahteVeri);
+  const [aramaKriterleri, setAramaKriterleri] = useState("");
 
   const gonderiyiBegen = (gonderiID) => {
-    /*
-      Bu fonksiyon, belirli bir id ile gönderinin beğeni sayısını bir artırma amacına hizmet eder.
-
-      Uygulamanın durumu, React ağacının en üstünde bulunur, ancak iç içe geçmiş bileşenlerin stateleri değiştirememesi adil olmaz!
-      Bu fonksiyon, belirli bir gönderinin beğeni sayısını artırılmasına olanak sağlamak amacıyla iç içe geçmiş bileşenlere aktarılır.
-
-	  "setGonderi" yi çağırın ve state ine "posts.map" çağrısını iletin.
-      `map` içine iletilen callback aşağıdaki mantığı gerçekleştirir:
-        - gönderinin idsi "gonderiID" ile eşleşirse, istenen değerlerle yeni bir gönderi nesnesi döndürün.
-        - aksi takdirde, sadece gönderi nesnesini değiştirmeden döndürün.
-     */
+    setGonderiler((x) => {
+      return x.map((gonderi) => {
+        if (gonderi.id === gonderiID) {
+          return {
+            ...gonderi,
+            begeniSayisi: gonderi.begeniSayisi + 1,
+          };
+        }
+        return gonderi;
+      });
+    });
   };
 
   return (
     <div className="App">
-      App Çalışıyor
-      {/* Yukarıdaki metni projeye başladığınızda silin*/}
-      {/* AramaÇubuğu ve Gönderiler'i render etmesi için buraya ekleyin */}
-      {/* Her bileşenin hangi proplara ihtiyaç duyduğunu kontrol edin, eğer ihtiyaç varsa ekleyin! */}
+      <AramaCubugu
+        aramaKriterleri={aramaKriterleri}
+        setAramaKriterleri={setAramaKriterleri}
+      />
+      <Gonderiler gonderiler={gonderiler} gonderiyiBegen={gonderiyiBegen} />
     </div>
   );
 };
